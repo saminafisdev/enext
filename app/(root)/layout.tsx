0bd3f77542c6e83468a457a3748103e211792d04
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-05-16 14:28:08 +0600

    feat: make the main layout full space

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main>{children}</main>
+      <main className="flex-1 flex flex-col p-4">{children}</main>
     </div>
   );
 };
